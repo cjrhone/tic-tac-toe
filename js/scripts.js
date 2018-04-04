b6793481d3e5f087d1a42f7e7ad6a84f69e403a3
CJ Rhone and Justin Roller <cj@>
2018-04-04 16:23:14 -0700

    Add X functionality and console log id of clicked column

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,53 @@
-var player1Turn = "X"
-var grid = ["1","2","3","4","5","6","7","8","9",]
-var gameOn = false;
+// const player1Turn = "X"
+// var grid = ["1","2","3","4","5","6","7","8","9",]
+// var gameOn = false;
+//
+// $(document).ready(function() {
+//   const grid = [
+//     [' ', ' ', ' ',],
+//     [' ', ' ', ' ',],
+//     [' ', ' ', ' ',]
+//   ];
+//
+//
+// $(".col").click(function() {
+//   $(this).html(player1Turn);
+//
+//
+// function winGameOver(gameArray){
+//   const winConditions = [
+//     [1,2,3],
+//     [4,5,6],
+//     [7,8,9],
+//     [1,4,7],
+//     [1,5,9],
+//     [3,5,7],
+//     [2,5,8],
+//     [3,6,9]
+//   ];
+// };
+//
+// });
+//
+// });
 
-$(document).ready(function() {
-  const grid = [
-    [' ', ' ', ' ',],
-    [' ', ' ', ' ',],
-    [' ', ' ', ' ',]
-  ];
+// EARLIER CODE
 
-$(".col").click(function() {
-  $(this).html(player1Turn);
+function playerTurn(turn, id) {
+  var spotTaken = $("#" + id).text();
+  if (spotTaken === "#") {
+    grid[id] = turn;
+    $("#" + id).text(turn);
+  }
+};
+
+function x(space) {
+  space = [];
+}
+
+function y(space) {
+  space=[];
+}
 
 function winGameOver(gameArray){
   const winConditions = [
@@ -25,6 +62,27 @@ function winGameOver(gameArray){
   ];
 };
 
-});
+var grid = ["1","2","3","4","5","6","7","8","9",]
 
+var turn = "X"
+
+
+$(document).ready(function() {
+   $(".col").click(function() {
+     $(this).html(turn);
+
+  var slot = $(this).attr('id');
+
+  if ( slot === "one" ) {
+    alert ("You clicked one");
+  } else {
+    alert ("THATS NOT ONE!");
+  }
+
+  console.log(slot)
+
+
+
+
+});
 });
